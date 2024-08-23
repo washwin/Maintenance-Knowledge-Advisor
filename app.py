0fc008eebb3aaf6e970d926ec832ae77d2ad8a59
washwin <114923029+washwin@users.noreply.github.com>
2024-08-23 17:45:11 +0530

    working apps

--- app.py
+++ app.py
@@ -2,22 +2,36 @@ import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
+from langchain_community.document_loaders import PyPDFLoader
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
 from langchain.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+from langchain_groq import ChatGroq
 from dotenv import load_dotenv
 
 load_dotenv()
+groq_api_key=os.getenv('GROQ_API_KEY')
 os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 
 
 
-
+def get_processed_docs(pdf_docs):
+    # loader=PyPDFLoader(pdf_docs)
+    # docs=loader.load() ## Document Loading
+    for pdf in pdf_docs:
+        loader=PyPDFLoader(pdf)
+        docs=loader.load()
+    text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200) ## Chunk Creation
+    final_documents=text_splitter.split_documents(docs[:20]) #splitting
+    return final_documents
 
 def get_pdf_text(pdf_docs):
     text=""
@@ -30,7 +44,7 @@ def get_pdf_text(pdf_docs):
 
 
 def get_text_chunks(text):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
     chunks = text_splitter.split_text(text)
     return chunks
 
@@ -42,47 +56,36 @@ def get_vector_store(text_chunks):
 
 
 def get_conversational_chain():
-
-    prompt_template = """
-    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
-    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
-    Context:\n {context}?\n
-    Question: \n{question}\n
-
-    Answer:
+    prompt=ChatPromptTemplate.from_template(
     """
-
-    model = ChatGoogleGenerativeAI(model="gemini-pro",temperature=0.3)
-
-    prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
+    Answer the questions based on the provided context only.
+    Please provide the most accurate response based on the question
+    <context>
+    {context}
+    <context>
+    Questions:{input}
+    
+    """
+    )
+    model=ChatGroq(groq_api_key=groq_api_key, model_name="mixtral-8x7b-32768")
     chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
-
     return chain
-
 
 
 def user_input(user_question):
     embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
-    
     new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
     docs = new_db.similarity_search(user_question)
-
     chain = get_conversational_chain()
-
-    
-    response = chain(
-        {"input_documents":docs, "question": user_question}
-        , return_only_outputs=True)
-
-    print(response)
-    st.write("Reply: ", response["output_text"])
+    response = chain({"input_documents":docs, "input": user_question}, return_only_outputs=True)
+    st.write("Solution : ", response["output_text"])
 
 
 
 
 def main():
     st.set_page_config("Chat PDF")
-    st.header("Chat with PDF using Gemini")
+    st.header("Maintenance Knowledge Advisor")
 
     user_question = st.text_input("Ask a Question from the PDF Files")
 
@@ -91,12 +94,13 @@ def main():
 
     with st.sidebar:
         st.title("Menu:")
-        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
+        pdf_docs = st.file_uploader("Upload new PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
         if st.button("Submit & Process"):
             with st.spinner("Processing..."):
-                raw_text = get_pdf_text(pdf_docs)
-                text_chunks = get_text_chunks(raw_text)
-                get_vector_store(text_chunks)
+                # final_docs = get_processed_docs(pdf_docs)
+                text=get_pdf_text(pdf_docs)
+                final_docs=get_text_chunks(text)
+                get_vector_store(final_docs)
                 st.success("Done")
 
 
